012503e81934c7ce15dc59ceb7d667c49aa85981
Lakshmi Narasiman <vigneshnarasiman@gmail.com>
2014-09-08 14:52:22 -0400

    Need to differentiate b/w 1st and 2nd case

--- ChatUp/src/com/org/chatup/showTiming.java
+++ ChatUp/src/com/org/chatup/showTiming.java
@@ -1,6 +1,8 @@
 package com.org.chatup;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.List;
 
 import javax.servlet.ServletException;
@@ -8,7 +10,10 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -19,24 +24,15 @@ import org.openqa.selenium.htmlunit.HtmlUnitDriver;
 /**
  * Servlet implementation class webScraper
  */
-@WebServlet("/webScraper")
+@WebServlet("/showTiming")
 public class showTiming extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-	private int  nth = -1;
 	
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		
-		//	Determine if user is searching for a movie or entering the movie number
-		//	Update nth
-		
-		//scrape("The Rifleman", nth);
-		scrape("Get Smart", nth);
-		//scrape("aksjbkvjsd", nth);
-		
 	}
 
 	/**
@@ -44,9 +40,50 @@ public class showTiming extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
+		
+		response.setContentType("application/json");
+		PrintWriter writer = response.getWriter();
+		HttpSession session = request.getSession();
+		
+		//	Get the JSON request in String format
+		StringBuilder sb = new StringBuilder();
+		BufferedReader br = request.getReader();
+		String str, resp = "";
+		while( (str = br.readLine()) != null ){
+		    sb.append(str);
+		}
+		
+		//	Get the actual command in String
+		String param="";
+		try {
+			JSONObject jsonReq = new JSONObject(sb.toString());
+			param = (String) jsonReq.get("request");
+			param = param.trim().toLowerCase();
+			
+		} catch (JSONException e1) {
+			e1.printStackTrace();
+		}
+		
+		if(session.isNew()) {	//	User entered movie name
+			session.setAttribute("movieTitle", param);
+			resp = "1st case\\n" + scrape(param, -1);
+		}
+		else {
+			if (param.matches("[0-9]+") && param.length() > 0) {
+				int nth = Integer.parseInt(param);
+				//session.removeAttribute("movieTitle");
+				resp = scrape(session.getAttribute("movieTitle").toString(), nth);
+			}
+		}
+
+		System.out.println(resp);
+		resp = "{\"response\":\"" + resp + "\"}";
+		
+		writer.print(resp);
+		writer.flush();
 	}
 	
-	protected void scrape(String title, int nth) {
+	protected String scrape(String title, int nth) {
 		WebDriver driver = new FirefoxDriver();
 
 		driver.navigate().to("http://xfinitytv.comcast.net/tv-listings");
@@ -56,54 +93,38 @@ public class showTiming extends HttpServlet {
 		searchButton.click();
 		
 		WebElement div = driver.findElement(By.cssSelector("div#searchList > *:first-child"));
-		
-		
 		String res = div.getAttribute("id").toString();
+		String retval = "";
 		
 		if(res.equals("noResults")) {
-			System.out.println("Your search did not match any results");
+			retval = "Your search did not match any results";
 		}
 		else {
 			List<WebElement> titleMatches = driver.findElements(By.cssSelector("div#searchList > *:first-child > *"));
-			
-			if(nth == -1) {		//	1st time: user searched movie name
 				
-				if(titleMatches.size() > 1) {
-					int count = 1;
-					for(WebElement temp : titleMatches) {
-						String t = temp.findElement(By.cssSelector("div.entity_info > h3 > a")).getAttribute("name");
-						System.out.println(count + ". " + t);
-						count++;
-					}
-					// Temp
-					titleMatches.get(0).findElement(By.cssSelector("div.entity_info > h3 > a")).click();
-					String timing = driver.findElement(By.cssSelector("#actionPanelTvListings > ul > li > div.details-info > div.record-time")).getText();					
-					System.out.println(timing);
+			if(titleMatches.size() > 1) {
+				int count = 1;
+				
+				for(WebElement temp : titleMatches) {
+					String t = temp.findElement(By.cssSelector("div.entity_info > h3 > a")).getAttribute("name");
+					retval += (count++) + ". " + t + "\\n";
 				}
-				else {
-					System.out.println("Only one match");
-					titleMatches.get(0).findElement(By.cssSelector("div.entity_info > h3 > a")).click();
-					
-					getShowTiming();
+				
+				if(nth > 0){
+					titleMatches.get(nth - 1).findElement(By.cssSelector("div.entity_info > h3 > a")).click();
+					retval = driver.findElement(By.cssSelector("#actionPanelTvListings > ul > li > div.details-info > div.record-time")).getText();
 				}
+				else if(nth == 0)
+					retval = "Invalid choice";
 			}
-			
 			else {
-				if((nth > 0) && (nth < titleMatches.size())) {
-					titleMatches.get(nth).findElement(By.cssSelector("div.entity_info > h3 > a")).click();
-					
-					getShowTiming();
-				}
-				else {
-					System.out.println("Invalid option selected");
-				}
+				System.out.println("Only one match");
+				titleMatches.get(0).findElement(By.cssSelector("div.entity_info > h3 > a")).click();
+				retval = driver.findElement(By.cssSelector("#actionPanelTvListings > ul > li > div.details-info > div.record-time")).getText();
 			}
 		}
 		
 		driver.close();
-	}
-	
-	protected void getShowTiming() {
-		System.out.println("Inside getShowTiming");
+		return retval;
 	}
 }
